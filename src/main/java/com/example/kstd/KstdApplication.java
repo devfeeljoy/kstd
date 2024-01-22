5745a6be564ad9d717f16b0110e724553f38fa65
woopil <jwp4465@highdev.pro>
2024-01-22 13:05:53 +0900

    init: initial commit

---
+++ src/main/java/com/example/kstd/KstdApplication.java
@@ -0,0 +1,13 @@
+package com.example.kstd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KstdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KstdApplication.class, args);
+    }
+
+}
